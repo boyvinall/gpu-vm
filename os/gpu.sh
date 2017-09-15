154bee7f44f902ae2003318174e26bcb3a6a2962
Matt Vinall <mattv@unity3d.com>
2017-09-15 09:58:04 +0100

    improve openstack vm script

--- os/gpu.sh
+++ os/gpu.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 # small test script to create GPU VMs on openstack
 
 GPU=${1:-gtx1080}
@@ -8,7 +8,7 @@ IMAGE=ubuntu-16.04-x86_64
 # <instance>
 function os_delete() {
   [ -f .$1 ] && (
-      . .$1
+      id=$(cat .$1 | gawk '$2 == "id"     { print $4 }')
       [ -n "$id" ] && openstack server delete $id
       rm -f .$1
   )
@@ -18,15 +18,22 @@ function os_delete() {
 function os_create() {
   FLAVOR_ID=$(openstack flavor list -f json | jq -r ".[] | select(.Name == \"$3\") | .ID")
   echo FLAVOR_ID $FLAVOR_ID
-  openstack server create --image $2 --flavor $FLAVOR_ID --network demo-net --key-name my-key -f shell $1 | tee .$1
+  openstack server create --image $2 --flavor $FLAVOR_ID --network demo-net --key-name my-key $1 | tee .$1
   while true; do
     echo -n .
-    . .$1
-    if [ "$status" != "BUILD" ]; then break; fi
+    id=$(cat .$1     | gawk '$2 == "id"     { print $4 }')
+    status=$(cat .$1 | gawk '$2 == "status" { print $4 }')
+    if [ $status != BUILD ]; then break; fi
     sleep 1
-    openstack server show -f shell $id > .$1
+    openstack server show $id > .$1
   done
-  openstack server add floating ip $id $4
+  if [ $status != ERROR ]; then
+    cat .$1
+    openstack server add floating ip $id $4
+  else
+    fault=$(cat .$1 | gawk '$2 == "fault" { print $4 }')
+    echo $fault
+  fi
 }
 
 # <instance> <image> <flavor> <ip>
@@ -41,6 +48,6 @@ function os_recreate() {
 # os_recreate gpu-both $IMAGE     m1.medium.$GPU 10.44.96.80
 # os_recreate win-gpu  win10-gpu  m1.medium.$GPU  10.44.96.61
 # os_recreate win-gpu  win10-gpu  m1.medium.$GPU  10.44.96.61
-os_recreate win  win10-gpu  m1.medium  10.44.96.54
+os_recreate win  win10-bokken  m1.large  10.44.96.54
 # os_recreate ubuntu-q35 ubuntu-16.04-x86_64-q35 m1.medium 10.44.96.59
 wait
